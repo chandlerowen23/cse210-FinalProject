367bae16ccd08299ce1aa2438a19d068acfded73
chirsmarti343 <christianjosue321@gmail.com>
2021-04-06 19:08:48 -0600

    private variable

--- Covid_Space_Shooter/game/redraw_window.py
+++ Covid_Space_Shooter/game/redraw_window.py
@@ -10,18 +10,18 @@ class Redraw_window:
         self.stage =Stage()
         
     def variables_window(self,lives,level,score):
-        self.lives = lives
-        self.level = level
-        self.score = score
+        self.__lives = lives
+        self.__level = level
+        self.__score = score
         
         
     def get_redraw(self):
         self.main_font = self.setup.get_main_font()
         constants.WIN.blit(constants.BG, (0,0))
         # draw text
-        lives_label = self.main_font.render(f"Lives: {self.lives}", 1, (255,255,255))
-        level_label = self.main_font.render(f"Level: {self.level}", 1, (255,255,255)) 
-        score_label = self.main_font.render(f"Score: {self.score}", 1, (255,255,255)) 
+        lives_label = self.main_font.render(f"Lives: {self.__lives}", 1, (255,255,255))
+        level_label = self.main_font.render(f"Level: {self.__level}", 1, (255,255,255)) 
+        score_label = self.main_font.render(f"Score: {self.__score}", 1, (255,255,255)) 
 
         constants.WIN.blit(lives_label, (10, 10))
         constants.WIN.blit(level_label, (constants.WIDTH - level_label.get_width() - 10, 10))
